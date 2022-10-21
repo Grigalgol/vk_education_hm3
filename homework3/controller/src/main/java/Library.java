d9afb639651d86a22dd3e1816c4133e6b6227c20
Gregory <grisha101202@yanex.ru>
2022-10-21 16:00:12 +0300

    refactor controller module

--- homework3/controller/src/main/java/Library.java
+++ homework3/controller/src/main/java/Library.java
@@ -1,12 +1,25 @@
 import entity.Book;
 import exceptions.EmptyCellException;
 import exceptions.SmallCapacityException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Library {
     private final Book[] cells;
 
-    public Library(int capacity) {
+    public Library(int capacity, @NotNull BooksFactory booksFactory) throws SmallCapacityException {
         cells = new Book[capacity];
+
+        List<Book> booksFromFile = new ArrayList<>(booksFactory.books());
+        if(booksFromFile.size() > capacity) {
+            throw new SmallCapacityException("the capacity is less than the number of books returned by the factory");
+        }
+
+        for (int i = 0; i < booksFromFile.size(); i++) {
+            cells[i] = booksFromFile.get(i);
+        }
     }
 
     public Book[] getCells() {
@@ -23,7 +36,7 @@ public class Library {
         return book;
     }
 
-    public void setBookInCell (Book book) throws EmptyCellException {
+    public void setBookInCell (@NotNull Book book) throws EmptyCellException {
         int numberEmptyCell = findEmptyCell();
         if(numberEmptyCell == -1) throw new EmptyCellException("the empty cell does not exist");
         else cells[numberEmptyCell] = book;
--- homework3/controller/src/main/java/LibraryFactory.java
+++ homework3/controller/src/main/java/LibraryFactory.java
@@ -7,22 +7,10 @@ import java.util.List;
 
 public class LibraryFactory {
 
-    private static BooksFactory booksFactory = new FileBookFactory("books.txt");
+    private static BooksFactory booksFactory = new FileBookFactory("books.txt"); //todo изменить
 
     public static Library library(int capacity) throws SmallCapacityException {
-        Library library = new Library(capacity);
-        Book[] books = library.getCells();
-
-        List<Book> booksFromFile = new ArrayList<>(booksFactory.books());
-        if(booksFromFile.size() > capacity) {
-            throw new SmallCapacityException("the capacity is less than the number of books returned by the factory");
-        }
-
-        for (int i = 0; i < booksFromFile.size(); i++) {
-            books[i] = booksFromFile.get(i);
-        }
-
-        return library;
+        return new Library(capacity, booksFactory);
     }
 
 }
