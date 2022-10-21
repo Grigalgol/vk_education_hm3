9c3df715a8837fff47237ee205dcd521874e9d53
Gregory <grisha101202@yanex.ru>
2022-10-21 11:45:53 +0300

    add .gitignore and editroot build.gradle

--- homework3/build.gradle.kts
+++ homework3/build.gradle.kts
@@ -5,14 +5,20 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+allprojects {
+    apply(plugin = "java")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+        implementation("org.jetbrains:annotations:13.0")
+    }
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-}
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
